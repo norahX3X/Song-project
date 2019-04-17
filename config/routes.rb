f61657c73639e32947c766c3f3c9f45cb833e929
Alshemimri <Alshemimri@gmail.com>
2019-04-17 14:27:19 +0300

    my commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'songs/index'
+  get 'songs/new'
+  get 'songs/edit'
+  get 'songs/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
