54ea0ef940ba86c893688d616ee43629f361d809
muneraabdul <Muneraabdul96@hotmail.com>
2019-04-17 15:14:47 +0300

    controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
- resource :songs
- resource :albums
- resource :artists
+ resources :songs
+ resources :albums
+ resources :artists
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
